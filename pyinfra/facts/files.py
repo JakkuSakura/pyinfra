8d2d776b321cbea50ef0811ae732ca4618706249
JakkuSakura <qjk2001@gmail.com>
2024-09-04 22:47:00 +0800

    fix FindFilesBase tests

--- pyinfra/facts/files.py
+++ pyinfra/facts/files.py
@@ -322,7 +322,60 @@ class FindFilesBase(FactBase):
     def process(self, output):
         return output
 
-    def command(self, path, quote_path=True):
+    def command(self,
+                path: str,
+                size: Optional[str | int] = None,
+                min_size: Optional[str | int] = None,
+                max_size: Optional[str | int] = None,
+                maxdepth: Optional[int] = None,
+                fname: Optional[str] = None,
+                iname: Optional[str] = None,
+                regex: Optional[str] = None,
+                args: List[str] = [],
+                quote_path=True
+                ):
+        """
+        @param path: the path to start the search from
+        @param size: exact size in bytes or human-readable format.
+                     GB means 1e9 bytes, GiB means 2^30 bytes
+        @param min_size: minimum size in bytes or human-readable format
+        @param max_size: maximum size in bytes or human-readable format
+        @param maxdepth: maximum depth to descend to
+        @param fname: True if the last component of the pathname being examined matches pattern.
+                      Special shell pattern matching characters (“[”, “]”, “*”, and “?”)
+                      may be used as part of pattern.
+                      These characters may be matched explicitly
+                      by escaping them with a backslash (“\\”).
+
+        @param iname: Like -name, but the match is case insensitive.
+        @param regex: True if the whole path of the file matches pattern using regular expression.
+        @param args: additional arguments to pass to find
+        @param quote_path: if the path should be quoted
+        @return:
+        """
+
+        """
+        Why we need special handling for size:
+        https://unix.stackexchange.com/questions/275925/why-does-find-size-1g-not-find-any-files
+        In short, 'c' means bytes, without it, it means 512-byte blocks.
+        If we use any units other than 'c', it has a weird rounding behavior,
+        and is implementation-specific. So, we always use 'c'
+        """
+        if min_size is not None:
+            args.append("-size +{0}c".format(parse_size(min_size)))
+        if max_size is not None:
+            args.append("-size -{0}c".format(parse_size(max_size)))
+        if size is not None:
+            args.append("-size {0}c".format(parse_size(size)))
+        if maxdepth is not None:
+            args.append("-maxdepth {0}".format(maxdepth))
+        if fname is not None:
+            args.append("-name {0}".format(QuoteString(fname)))
+        if iname is not None:
+            args.append("-iname {0}".format(QuoteString(iname)))
+        if regex is not None:
+            args.append("-regex {0}".format(QuoteString(regex)))
+
         return make_formatted_string_command(
             "find {0} -type {type_flag} || true",
             QuoteString(path) if quote_path else path,
